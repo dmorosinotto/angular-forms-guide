e631854a3fef27399da5a4e53aa2100d12e5a860
Daniele Morosinotto <d.morosinotto@icloud.com>
2024-03-19 16:24:32 +0100

    Remove tests

--- src/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: "jest-preset-angular",
-  displayName: "unit",
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-};
--- src/setup-jest.ts
+++
@@ -1,7 +0,0 @@
-import 'jest-preset-angular/setup-jest';
-import '@testing-library/jest-dom';
-import { configure } from '@testing-library/angular';
-
-configure({
-  excludeComponentDeclaration: true,
-});
